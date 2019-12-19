4040f4bcb254d6a8fca0a63805267ad4fdb04663
vatseyb <bhuvivatsey@gmail.com>
2019-12-19 01:11:20 -0500

    BubbleSort with Generics

---
+++ src/com/learning/java/functional/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.learning.java.functional;
+
+import java.util.Scanner;
+
+public class BubbleSort <Int>{
+
+    public void Sortarray(Int arr[]) {
+
+        int n = arr.length;
+        Int temp=arr[0];
+        for (int i=0;i<n-1;i++) {
+            for(int j=0;j<n-i-1;j++) {
+                if (arr[j] > arr[j+1]) {
+                    temp=arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner scanarray = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int n = scanarray.nextInt();
+        //int[] Array1 = new int[n];
+        BubbleSort <Integer> [] Array1 = new BubbleSort[n];
+        System.out.println("Enter the array elements");
+        for (int i=0;i<n;i++)
+        {
+            Array1[i] =  scanarray.nextInt();
+        }
+        BubbleSort ob = new BubbleSort();
+        ob.Sortarray(Array1);
+        System.out.println("Array:");
+        for (int i=0;i<n;i++)
+        {
+            System.out.print(Array1[i]+" ");
+        }
+    }
+}
